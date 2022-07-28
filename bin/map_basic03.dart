66d4ffb27047e23ad0cbd9b3e6d53cc93e885601
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-28 13:44:57 +0500

    03

--- bin/map_basic03.dart
+++ bin/map_basic03.dart
@@ -1,18 +1,15 @@
 /*
   
-  Given an argument type of Map.
-Return true if the length of the map is greater than zero, otherwise false.
-
 
 */
-
-bool func(Map dct) {
+bool func(Map <int,String>dct){
   if(0<dct.length){
     return true;
   }
   return false;
 }
 
-void main() {
-  print(func({2: 'Ozodbek',5: 'Salohiddin'}));
+void main(){
+
+  print(func({2:'Ozodbek',5:'Salohiddin'}));
 }
