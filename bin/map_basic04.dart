7ef09212d2a899f5d89a1afa9d00ca21fe049144
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-28 13:51:34 +0500

    04

--- bin/map_basic04.dart
+++ bin/map_basic04.dart
@@ -7,10 +7,16 @@ Return list_to_map variable
 
 */
 
-Map func(List lst1, List lst2) {
-  return;
+void main() {
+  print(func([2, 4, 5, 6], ['one', 'two', 'three', 'four']));
 }
 
-void main() {
-  print(func());
+Map func(List lst1, List lst2) {
+  int i = 0;
+  Map data = {};
+  while (i < lst1.length) {
+    data[lst1[i]] = lst2[i];
+    i++;
+  }
+  return data;
 }
